c8ec7df4f2eecb04f048ccc56af80e67e1799f7b
ssimonton007 <55812276+ssimonton007@users.noreply.github.com>
2022-08-18 09:20:05 -0700

    Added to repository

---
+++ Shared/UploadedBookRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordSmithWeb.Shared
+{
+    public class UploadedBookRequest
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string BookFileName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public byte[] BookContent { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string IgnoredWordsFileName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public byte[] IgnoreWordsContent { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int RequestedResults { get; set; }
+    }
+}
---
+++ Shared/WordResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordSmithWeb.Shared
+{
+    public class WordResult
+    {
+        public WordResult(){}
+
+        public WordResult(string strWord, int intCount)
+        {
+            word = strWord;
+            count = intCount;
+        }
+
+        public string word { get; set; }
+
+        public int count { get; set; }
+    }
+}
